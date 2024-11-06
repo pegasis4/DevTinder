8bd005d2b90ea776cdb82e427e20ea02209ef4e0
Yash Padiyar <yashucan4@gmail.com>
2024-11-06 16:45:48 +0530

    handling dynamic routes

--- src/app.js
+++ src/app.js
@@ -4,9 +4,12 @@ const app=express()
 app.use("/test",(req,res)=>{
     res.send("hello hii")
 })
-app.get("/user",(req,res)=>{
+app.get("/user/:id",(req,res)=>{
+    const res1=req.params
+    console.log(res1)    
     res.send({"name":"yash","age":"20"})
 })
+
 app.post("/user",(req,res)=>{
     res.send("save data to db")
 })
