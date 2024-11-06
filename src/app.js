a0bf25655d492ae5effc6595c33d8da335f392c7
Yash Padiyar <yashucan4@gmail.com>
2024-11-06 16:32:54 +0530

    exploring get,post,commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const app=express()
 app.use("/test",(req,res)=>{
     res.send("hello hii")
 })
+app.get("/user",(req,res)=>{
+    res.send({"name":"yash","age":"20"})
+})
+app.post("/user",(req,res)=>{
+    res.send("save data to db")
+})
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
